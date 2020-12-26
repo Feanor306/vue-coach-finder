225e624e4d09b1418c0440dccfbd391213f930d5
Feanor306 <feanor306@live.com>
2020-12-26 11:33:20 +0100

    Init commit. Au

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Page not found</h2>
+      <p>
+        This page could not be found. You can check the list of
+        <router-link to="/coaches">coches.</router-link>
+      </p>
+    </base-card>
+  </section>
+</template>
